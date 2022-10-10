2eececf4f95f1f4029dc4ec005d3263a8dae48be
iron-strix <88483614+iron-strix@users.noreply.github.com>
2022-10-09 20:44:02 -0500

    first commit

---
+++ src/router.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import HomePage from '@/views/HomePage.vue'
+
+const routes = [
+  { path: '/', name: 'Home', component: HomePage },
+  {
+    path: '/character/:id',
+    name: 'Character',
+    component: () => import('@/views/CharacterDetails.vue'),
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
---
+++ src/views/HomePage.vue
@@ -0,0 +1,25 @@
+<script setup>
+import BaseCard from '@/components/BaseCard.vue'
+import useCharacters from '@/composables/useCharacters'
+
+const { characters, fetchCharacters } = useCharacters()
+</script>
+
+<template>
+  <button
+    title="Load Characters"
+    class="z-90 fixed bottom-8 right-8 flex h-20 w-20 items-center justify-center rounded-full bg-green-600 text-4xl text-white drop-shadow-lg duration-300 hover:scale-110 hover:bg-green-700 hover:drop-shadow-2xl"
+    @click="fetchCharacters"
+  >
+    🚀
+  </button>
+  <main class="min-h-screen bg-gradient-to-r from-fuchsia-900 to-red-700">
+    <div class="container mx-auto grid grid-cols-4 gap-4 py-8">
+      <BaseCard
+        v-for="character in characters"
+        :key="character._id"
+        :character="character"
+      />
+    </div>
+  </main>
+</template>
