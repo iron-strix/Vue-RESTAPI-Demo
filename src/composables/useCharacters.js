94ab2fb439981fd3db934711fc6b35bb4471075f
iron-strix <88483614+iron-strix@users.noreply.github.com>
2022-10-09 21:10:52 -0500

    added paging to useCharacters

--- src/composables/useCharacters.js
+++ src/composables/useCharacters.js
@@ -3,11 +3,17 @@ import useApi from '@/composables/useAPI'
 
 const characters = ref([])
 const api = useApi()
+const page = ref(1)
 
 const useCharacters = () => {
   const fetchCharacters = async () => {
-    const { data } = await api.instance.get('/characters')
+    const { data } = await api.instance.get('/characters', {
+      params: {
+        page: page.value,
+      },
+    })
     characters.value.push(...data.data)
+    page.value++
   }
   return { characters, fetchCharacters }
 }
